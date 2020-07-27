c0db2595c7c00fa1ac54ba06c69abf7804724bf1
de-cryptor <bim2014001@iiita.ac.in>
2020-07-28 00:06:19 +0530

    API endpoints updated

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from django.urls import path
-from store.views import BoxView,AuthTokenInterface
+from store.views import *
+from rest_framework.authtoken import views
 
 urlpatterns = [
 
-    path('auth_login/',AuthTokenInterface.as_view(),name='auth-token'),
-    path('box_view/',BoxView.as_view(),name='box-view')
+    path('auth_token/',views.obtain_auth_token,name='auth-token'),
+    path('box/create/',create_box,name='create-box'),
+    path('box/list/',list_box,name='list-box'),
+    path('box/my_list/',my_list_box,name='list-my-box'),
+    path('box/update/<str:pk>',update_box,name='update-box'),
+    path('box/delete/<str:pk>',delete_box,name='delete-box')
+
 ]
